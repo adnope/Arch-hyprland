2de61b0a08cce25835ffe96ce54cb7920b252ab1
adnope <anhduyxh7@gmail.com>
2025-05-21 14:04:14 +0700

    Added gtk theme and finish waybar

--- assets/copy_script.sh
+++ assets/copy_script.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # Copy fonts
-sudo mkdir -p "/usr/local/share/fonts/ttf"
-sudo cp -r "$SCRIPT_DIR"/fonts/* "/usr/local/share/fonts/ttf"
+sudo mkdir -p /usr/local/share/fonts/ttf
+sudo cp -r fonts/* /usr/local/share/fonts/ttf
 sudo fc-cache
 
 # Copy grub background & font
-sudo cp "$SCRIPT_DIR"/grub-theme/* "/boot/grub"
+sudo cp grub-theme/* /boot/grub
 
 # Copy sddm theme
-sudo cp -r "$SCRIPT_DIR"/sddm-theme/KDE-Story-Dark-SDDM-6 "/usr/share/sddm/themes"
+sudo cp -r sddm-theme/KDE-Story-Dark-SDDM-6 /usr/share/sddm/themes
 
+# Copy cursors and GTK theme
 mkdir -p "$HOME"/.local/share/icons
-for archive in "$SCRIPT_DIR"/cursors/*; do
-    tar -xf "$archive" -C "$HOME"/.local/share/icons
-done
+mkdir -p "$HOME"/.local/share/themes
+cp -r gtk-theme/* "$HOME"/.local/share/themes
+cp -r cursors/* "$HOME"/.local/share/icons
